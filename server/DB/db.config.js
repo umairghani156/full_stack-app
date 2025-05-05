a859631973601642a486b972f2b7e815da30ae98
umairghani156 <umairghani156@gmail.com>
2025-05-05 08:09:25 -0700

    added.

--- server/DB/db.config.js
+++ server/DB/db.config.js
@@ -1,7 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-    log: ["query"]
-});
+const globalForPrisma = globalThis;
 
-export default prisma
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;

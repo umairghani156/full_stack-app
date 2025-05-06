dd19e1c342419e7bc2a730304e5867974dc4679f
umairghani156 <umairghani156@gmail.com>
2025-05-06 03:39:38 -0700

    added.

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const io = new Server(server, {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://full-stack-app-8xhr.vercel.app"],
     credentials: true
 }));
 
